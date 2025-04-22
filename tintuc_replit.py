172a36eeeb4c72dc4ca6d5969fc1b11d949e7a66
narutovt159 <59289532+narutovt159@users.noreply.github.com>
2025-04-22 14:26:56 +0700

    Update tintuc_replit.py

--- tintuc_replit.py
+++ tintuc_replit.py
@@ -27,13 +27,15 @@ last_message_id = {}
 def init_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Tắt giao diện đồ họa
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-software-rasterizer")
     chrome_options.add_argument("--no-cache")  # Vô hiệu hóa cache
     chrome_options.add_argument("--disable-gpu")  # Tắt GPU để giảm tải
     chrome_options.add_argument("--disable-extensions")  # Tắt extensions không cần thiết
+    chrome_options.add_argument("--remote-debugging-port=9222")  # Thêm tùy chọn remote debugging
+    chrome_options.add_argument("--no-sandbox")  # Tắt sandboxing (có thể cần cho môi trường server hoặc Docker)
+
+    # Cài đặt driver với ChromeDriverManager và các tùy chọn trên
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
     return driver
 
 # Hàm lấy giá coin từ crypto.com
