cafa54a4a461f64ad5349a078832abaa01c3ffd7
narutovt159 <59289532+narutovt159@users.noreply.github.com>
2025-04-22 11:53:40 +0700

    Update tintuc_replit.py

--- tintuc_replit.py
+++ tintuc_replit.py
@@ -27,6 +27,7 @@ last_message_id = {}
 def init_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-cache")  # Vô hiệu hóa cache
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
