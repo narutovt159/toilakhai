fffba8b6de09d5cf6d3521d77ba14ed3c161b1ad
narutovt159 <59289532+narutovt159@users.noreply.github.com>
2025-04-26 02:27:38 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.Popen(["python3", "tintuc_test.py"])
+subprocess.Popen(["python3", "tinmoi.py"])
+subprocess.Popen(["python3", "positions.py"])
+subprocess.Popen(["python3", "giacoin.py"])
+subprocess.Popen(["python3", "bitcoin_news.py"])
